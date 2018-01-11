5c16dbc2cfc962fc469436e43957f93bc9a25a5f
SebLu1 <sl767@cam.ac.uk>
2018-01-11 22:25:02 +0000

    ct picture opt

--- AR_for_CT.py
+++ AR_for_CT.py
@@ -15,6 +15,7 @@ import platform
 
 class data_preprocessing(object):
     model_name = 'none'
+    source = 'ellipses'
 
     @staticmethod
     def to_uint(pic):
@@ -58,8 +59,8 @@ class data_preprocessing(object):
                 pass
 
 
-    def __init__(self, source = 'ellipses'):
-        if source == 'LUNA':
+    def __init__(self):
+        if self.source == 'LUNA':
             name = platform.node()
             Train_Path = ''
             Eval_Path = ''
@@ -104,7 +105,7 @@ class data_preprocessing(object):
     # returns simulated measurement, original pic and fbp
     def generate_data(self, batch_size, validation=False, type = 'ellipses'):
         """Generate a set of random data."""
-        n_generate = 1 if validation else batch_size
+        n_generate = 1 if (validation and type=='ellipses') else batch_size
 
         y = np.empty((n_generate, self.operator.range.shape[0], self.operator.range.shape[1], 1), dtype='float32')
         x_true = np.empty((n_generate, self.space.shape[0], self.space.shape[1], 1), dtype='float32')
@@ -115,10 +116,14 @@ class data_preprocessing(object):
                 if type=='ellipses':
                     phantom = odl.phantom.shepp_logan(self.space, True)
                 elif type == 'LUNA':
-                    path = self.get_random_path()
+                    path = self.get_random_path(validation=True)
                     phantom = self.space.element(self.get_pic(path))
             else:
-                phantom = self.random_phantom(self.space)
+                if type == 'LUNA':
+                    path = self.get_random_path(validation=False)
+                    phantom = self.space.element(self.get_pic(path))
+                else:
+                    phantom = self.random_phantom(self.space)
             data = self.operator(phantom)
             noisy_data = data + odl.phantom.white_noise(self.operator.range) * np.mean(np.abs(data)) * 0.05
 
@@ -148,8 +153,11 @@ class data_preprocessing(object):
         return odl.phantom.ellipsoid_phantom(spc, ellipses)
 
     # methodes for obtaining the medical data
-    def get_random_path(self):
-        path = self.training_list[random.randint(0, self.training_list_length-1)]
+    def get_random_path(self, validation =False):
+        if not validation:
+            path = self.training_list[random.randint(0, self.training_list_length-1)]
+        else:
+            path = self.eval_list[random.randint(0, self.eval_list_length - 1)]
         return path
 
     # checks if the xml file is a valid source (has readingSessions instead of CXRreadingSessions)
@@ -169,8 +177,6 @@ class data_preprocessing(object):
         pic = pic/ np.amax(pic)
         return pic
 
-class post_processing(data_preprocessing):
-    pass
 
 class ct_recon(data_preprocessing):
     model_name = 'default'
@@ -202,7 +208,7 @@ class ct_recon(data_preprocessing):
         else:
             print('No save found')
 
-    def tv_reconsruction(self, y, param = 0.001):
+    def tv_reconstruction(self, y, param = 0.001):
         # the operators
         gradients = odl.Gradient(self.space, method='forward')
         broad_op = odl.BroadcastOperator(self.operator, gradients)
@@ -609,62 +615,10 @@ class Recon1(ct_recon):
         output = tf.matmul(p2resh, weights[8]) + weights[9]
         return output
 
-class Recon2(ct_recon):
-    model_name = 'Recon2'
+class Recon_LUNA(Recon1):
+    model_name = 'Recon_LUNA'
+    source = 'LUNA'
 
-    def get_weights(self):
-        con1 = tf.get_variable(name="conv1_ad", shape=[5, 5, 16, 32],
-                               initializer=(
-                                   tf.contrib.layers.xavier_initializer_conv2d(uniform=False, dtype=tf.float32)))
-        bias1 = tf.Variable(tf.constant(0.1, shape=[1, 1, 1, 32]), name="bias1_ad")
-        con2 = tf.get_variable(name="conv2_ad", shape=[5, 5, 32, 64],
-                               initializer=(
-                                   tf.contrib.layers.xavier_initializer_conv2d(uniform=False, dtype=tf.float32)))
-        bias2 = tf.Variable(tf.constant(0.1, shape=[1, 1, 1, 64]), name="bias2_ad")
-        con3 = tf.get_variable(name="conv3_ad", shape=[5, 5, 64, 128],
-                               initializer=(
-                                   tf.contrib.layers.xavier_initializer_conv2d(uniform=False, dtype=tf.float32)))
-        bias3 = tf.Variable(tf.constant(0.1, shape=[1, 1, 1, 128]), name="bias3_ad")
-        con4 = tf.get_variable(name="conv4_ad", shape=[5, 5, 128, 128],
-                               initializer=(
-                                   tf.contrib.layers.xavier_initializer_conv2d(uniform=False, dtype=tf.float32)))
-        bias4 = tf.Variable(tf.constant(0.1, shape=[1, 1, 1, 128]), name="bias4_ad")
-        logits_W = tf.get_variable(name="logits_W_ad", shape=[128 * 8 * 8, 1],
-                                   initializer=(
-                                       tf.contrib.layers.xavier_initializer(uniform=False, dtype=tf.float32)))
-        logits_bias = tf.Variable(tf.constant(0.0, shape=[1, 1]), name='logits_bias_ad')
-        con_pre = tf.get_variable(name="conv_pre", shape=[5, 5, 1, 16],
-                                  initializer=(
-                                      tf.contrib.layers.xavier_initializer_conv2d(uniform=False, dtype=tf.float32)))
-        bias_pre = tf.Variable(tf.constant(0.1, shape=[1, 1, 1, 16]), name="bias_pre")
-
-        return [con1, bias1, con2, bias2, con3, bias3, con4, bias4, logits_W, logits_bias, con_pre, bias_pre]
-
-    def wasserstein_network(self, weights, input_pic):
-        # convolutional layer without downsampling
-        conv_pre = lrelu(tf.nn.conv2d(input_pic, weights[10], strides=[1, 1, 1, 1], padding='SAME') + weights[11])
-
-        # 1st convolutional layer (pic size 128)
-        conv1 = lrelu(tf.nn.conv2d(conv_pre, weights[0], strides=[1, 2, 2, 1], padding='SAME') + weights[1])
-
-        # 2nd conv layer (pic size 64)
-        conv2 = lrelu(tf.nn.conv2d(conv1, weights[2], strides=[1, 2, 2, 1], padding='SAME') + weights[3])
-
-        # 3rd conv layer (pic size 32)
-        conv3 = lrelu(tf.nn.conv2d(conv2, weights[4], strides=[1, 2, 2, 1], padding='SAME') + weights[5])
-
-        # 4th conv layer (pic size 16)
-        conv4 = lrelu(tf.nn.conv2d(conv3, weights[6], strides=[1, 2, 2, 1], padding='SAME') + weights[7])
-
-        # reshape (pic size 8)
-        p2resh = tf.reshape(conv4, [-1, 128 * 8 * 8])
-
-        # dropout layer
-        # drop = tf.layers.dropout(p2resh, 0.3, training=True)
-
-        # logits
-        output = tf.matmul(p2resh, weights[8]) + weights[9]
-        return output
 
 
 
--- CT.py
+++ CT.py
@@ -1,21 +1,26 @@
 import AR_for_CT as ar
 import numpy as np
 
-recon = ar.Recon1()
-# recon.pretrain_Wasser_FBP(500)
-# recon.pretrain_Wasser_DataMinimizer(500)
-# recon.find_good_lambda()
-# recon.find_noise_level()
-# recon.create_optimized_images(1, mu=2.5, steps=30, starting_point='Mini')
-#recon.create_optimized_images(64, mu=2, step_s=0.1, steps=200, starting_point='Mini')
 if 1:
-    for k in range(2):
-        recon.train(500, 250, starting_point='Mini')
+    recon = ar.Recon1()
+    # recon.pretrain_Wasser_FBP(500)
+    # recon.pretrain_Wasser_DataMinimizer(500)
+    recon.find_good_lambda()
+    # recon.find_noise_level()
+    # recon.create_optimized_images(1, mu=2.5, steps=30, starting_point='Mini')
+    #recon.create_optimized_images(64, mu=2, step_s=0.1, steps=200, starting_point='Mini')
+    if 0:
+        for k in range(2):
+            recon.train(500, 250, starting_point='Mini')
 
+
+    if 1:
+        starting_point = 'Mini'
+        recon.create_optimized_images(512, mu=2, step_s=0.1, steps=250, starting_point=starting_point)
 
 if 0:
-    starting_point = 'Mini'
-    recon.create_optimized_images(512, mu=2, step_s=0.1, steps=150, starting_point=starting_point)
+    recon = ar.Recon_LUNA()
+    recon.find_good_lambda()
 
 
 
