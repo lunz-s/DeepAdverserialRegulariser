4bc1d49048ee12bb7144256b5cd422be9c57bc85
SebLu1 <sl767@cam.ac.uk>
2018-01-29 17:11:51 +0000

    track indepenent layer

--- stacked_denoisers.py
+++ stacked_denoisers.py
@@ -1,9 +1,8 @@
 from iterative_denoising import stacked_denoiser
 
 sd = stacked_denoiser(2)
-sd.train_layer(1, 50)
+# sd.train_layer(1, 50)
+sd.independant_layer(1, 1)
 sd.independant_layer(1, 3)
 sd.independant_layer(1, 5)
 sd.independant_layer(1, 10)
-sd.independant_layer(1, 20)
-sd.independant_layer(1, 40)
