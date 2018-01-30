fc825733409b794787aa57b1b70a288af9235f58
SebLu1 <sl767@cam.ac.uk>
2018-01-30 11:36:13 +0000

    second layer training

--- stacked_denoisers.py
+++ stacked_denoisers.py
@@ -2,15 +2,15 @@ from iterative_denoising import stacked_denoiser
 from iterative_denoising import bregmann_denoiser
 
 
-if 1:
+if 0:
     sd = stacked_denoiser(2, mu=[44,5])
     sd.independant_layer(1, 10)
     sd.independant_layer(1, 15)
     sd.independant_layer(1, 25)
     sd.end()
 
-if 0:
-    sd = bregmann_denoiser(2, mu=[15, 15])
+if 1:
+    sd = bregmann_denoiser(2, mu=[20, 20])
     sd.train_layer(1, 500)
     sd.independant_layer(1, 10)
     sd.independant_layer(1, 20)
