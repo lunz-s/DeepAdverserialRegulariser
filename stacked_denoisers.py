4306f0ec7980adc60a1c34043f0f9e11c072ffcf
SebLu1 <sl767@cam.ac.uk>
2018-01-30 16:53:53 +0000

    third layer training

--- stacked_denoisers.py
+++ stacked_denoisers.py
@@ -4,15 +4,13 @@ from iterative_denoising import bregmann_denoiser
 
 if 0:
     sd = stacked_denoiser(2, mu=[44,5])
+    sd.train_layer(1, 500)
     sd.independant_layer(1, 10)
     sd.independant_layer(1, 15)
     sd.independant_layer(1, 25)
     sd.end()
 
 if 1:
-    sd = bregmann_denoiser(2, mu=[25, 25])
-    sd.train_layer(1, 500)
-    sd.independant_layer(1, 10)
-    sd.independant_layer(1, 20)
-    sd.independant_layer(1,30)
+    sd = bregmann_denoiser(3, mu=[25, 25, 25])
+    sd.train_layer(2, 500)
     sd.end()
