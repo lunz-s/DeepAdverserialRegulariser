df17a93af8f3933bd15fe47f42fb6a3f556e8a57
SebLu1 <sl767@cam.ac.uk>
2018-02-21 10:10:51 +0000

    bug fixing

--- networks.py
+++ networks.py
@@ -36,6 +36,7 @@ class binary_classifier(object):
         finishing_size = int(self.size[0]* self.size[1]/(32*32))
         dimensionality = finishing_size * finishing_size * 128
         reshaped = tf.reshape(pool6, [-1, dimensionality])
+        print('Dimension: ' + str(self.size))
         print('reshape dimension: ' + str(dimensionality))
 
         # dense layer for classification
