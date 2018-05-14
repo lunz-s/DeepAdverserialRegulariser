8a7b6a279587dda0efb2e80021213f10803bf8d7
SebLu1 <sl767@cam.ac.uk>
2018-05-14 09:42:32 +0100

    ellipses added

--- LUNA.py
+++ LUNA.py
@@ -247,6 +247,20 @@ if number == 6.0:
             adv_reg.pretrain_Wasser_DataMinimizer(500)
         adv_reg.evaluate_image_optimization(steps=70)
 
+    if n == 1.1:
+        adv_reg = low_noise_ar()
+        adv_reg.find_good_lambda()
+
+        repeat = 1
+        while repeat == 1:
+            ss = input('Please insert desired steps size: ')
+            a_s = input('Please insert amount of steps: ')
+            mu = input('Please insert regularisation parameter mu: ')
+            adv_reg.evaluate_image_optimization(batch_size=32, mu=mu, step_s=ss,
+                                                steps=a_s, starting_point='Mini')
+            repeat = input('Repeat experiment?')
+        adv_reg.end()
+
     if n ==2:
         recon = low_noise_pp()
         print(recon.noise_level)
@@ -306,6 +320,112 @@ if number == 6.0:
             plt.savefig(path + str(k) + '.png')
             plt.close()
 
+# experiments on low noise level
+if number == 7.0:
+    nl_el = 0.02
+    ### Comparison experiments: Standard architecture
+    class ar(adversarial_regulariser):
+        experiment_name = 'ConvNet'
+        noise_level = nl_el
+        mu_default = .2
+        learning_rate = 0.0001
+        step_size = 1
+        total_steps_default = 25
+        default_sampling_pattern = 'startend'
+
+        def get_network(self, size, colors):
+            return improved_binary_classifier(size=size, colors=colors)
+
+        def unreg_mini(self, y, fbp):
+            return self.update_pic(15, 1, y, fbp, 0)
+
+        def get_Data_pip(self):
+            return ellipses()
+
+    class tv(total_variation):
+        experiment_name = 'Standard'
+        noise_level = nl_el
+
+        def get_Data_pip(self):
+            return ellipses()
+
+    class pp(postprocessing):
+        experiment_name = 'Standard'
+        noise_level = nl_el
+        def_lambda = 0.0007
+
+        def get_Data_pip(self):
+            return ellipses()
+
+    n = input('exp: ')
+
+    if n == 1:
+        # create object of type experiment1
+        adv_reg = ar()
+        adv_reg.set_total_steps(30)
+        # adv_reg.find_good_lambda()
+        for k in range(5):
+            adv_reg.pretrain_Wasser_DataMinimizer(500)
+        adv_reg.evaluate_image_optimization(steps=70)
+
+    if n ==2:
+        recon = pp()
+        print(recon.noise_level)
+        for k in range(5):
+            recon.train(500)
+
+    if n == 3:
+        tv = tv()
+        print(tv.noise_level)
+        lmb = []
+        for k in range(10):
+            lmb.append(0.0002 * (k + 1))
+        tv.find_TV_lambda(lmb)
+
+    if n == 4:
+        batch_size = 32
+        ar = ar()
+        y, x_true, fbp = ar.generate_training_data(batch_size=batch_size, training_data=False)
+        ar_results = ar.evaluate(y, fbp)
+        for res in ar_results:
+            print('AR: ' + str(quality(x_true, res)))
+        ar.end()
+        pp = pp()
+        pp_results = pp.evaluate(y, fbp)
+        print('PP: ' + str(quality(x_true, pp_results)))
+        pp.end()
+        tv = tv()
+        tv_results = tv.evaluate(y, fbp)
+        print('TV: ' + str(quality(x_true, tv_results)))
+        tv.end()
+        print('FBP: ' + str(quality(x_true, fbp)))
+
+        for k in range(10):
+            plt.figure()
+            plt.subplot(151)
+            plt.imshow(ut.cut_image(x_true[k, ..., 0]), cmap='Greys')
+            plt.axis('off')
+            plt.title('Ground_truth')
+            plt.subplot(152)
+            plt.imshow(ut.cut_image(fbp[k, ..., 0]), cmap='Greys')
+            plt.axis('off')
+            plt.title('FBP')
+            plt.subplot(153)
+            plt.imshow(ut.cut_image(pp_results[k, ..., 0]), cmap='Greys')
+            plt.title('PostProcessing')
+            plt.axis('off')
+            plt.subplot(154)
+            plt.imshow(ut.cut_image((ar_results[20])[k, ..., 0]), cmap='Greys')
+            plt.title('Adv. Reg')
+            plt.axis('off')
+            plt.subplot(155)
+            plt.imshow(ut.cut_image(tv_results[k, ..., 0]), cmap='Greys')
+            plt.title('TV')
+            plt.axis('off')
+            path = '/local/scratch/public/sl767/DeepAdversarialRegulariser/Saves/Computed_Tomography/LUNA/Comparison_LowNoise/'
+            ut.create_single_folder(path)
+            plt.savefig(path + str(k) + '.png')
+            plt.close()
 
 
 if number == 10.0:
